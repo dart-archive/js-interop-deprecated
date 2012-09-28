b06c711f3e6ce0f584ed196ed5a12421141ca1d4
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2012-09-28 18:14:14 +0200

    modifications after vsmenon review

--- lib/js.dart
+++ lib/js.dart
@@ -681,16 +681,16 @@ class Proxy {
    * JavaScript [constructor].  The arguments should be either
    * primitive values, DOM elements, or Proxies.
    */
-  factory Proxy(constructor, [arg1, arg2, arg3, arg4]) => new Proxy.withVarargs(constructor, [arg1, arg2, arg3, arg4]);
+  factory Proxy(constructor, [arg1, arg2, arg3, arg4]) => new Proxy.withArgList(constructor, [arg1, arg2, arg3, arg4]);
 
   /**
    * Constructs a [Proxy] to a new JavaScript object by invoking a (proxy to a)
-   * JavaScript [constructor].  The arguments list should contains either
+   * JavaScript [constructor].  The [arguments] list should contain either
    * primitive values, DOM elements, or Proxies.
    */
-  factory Proxy.withVarargs(constructor, List args) {
+  factory Proxy.withArgList(constructor, List arguments) {
     if (_depth == 0) throw 'Cannot create Proxy out of scope.';
-    final serialized = ([constructor]..addAll(args)).map(_serialize);
+    final serialized = ([constructor]..addAll(arguments)).map(_serialize);
     final result = _jsPortCreate.callSync(serialized);
     return _deserialize(result);
   }
