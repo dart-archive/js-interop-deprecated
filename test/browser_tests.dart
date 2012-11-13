13de5afe1d87c19985b0deb2ab3d3c14fa8f4b87
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2012-11-13 18:10:21 +0100

    allow instanciation of JS Date and add some tests for native Js objects

--- test/browser_tests.dart
+++ test/browser_tests.dart
@@ -26,7 +26,7 @@ main() {
   });
 
 
-  test('instanciation of Array', () {
+  test('js instanciation : new Array()', () {
     js.scoped(() {
       final a = new js.Proxy(js.context.Array);
       expect(a, isNotNull);
@@ -41,13 +41,57 @@ main() {
     });
   });
 
-  test('instanciation of Object', () {
+  test('js instanciation : new Date()', () {
+    js.scoped(() {
+      final a = new js.Proxy(js.context.Date);
+      expect(a.getTime(), isNotNull);
+    });
+  });
+
+  test('js instanciation : new Date(12345678)', () {
+    js.scoped(() {
+      final a = new js.Proxy(js.context.Date, 12345678);
+      expect(a.getTime(), equals(12345678));
+    });
+  });
+
+  test('js instanciation : new Date("December 17, 1995 03:24:00")', () {
+    js.scoped(() {
+      final a = new js.Proxy(js.context.Date, "December 17, 1995 03:24:00");
+      expect(a.getTime(), equals(819167040000));
+    });
+  });
+
+  test('js instanciation : new Date(1995,11,17)', () {
+    js.scoped(() {
+      final a = new js.Proxy(js.context.Date, 1995,11,17);
+      expect(a.getTime(), equals(819154800000));
+    });
+  });
+
+  test('js instanciation : new Date(1995,11,17,3,24,0)', () {
+    js.scoped(() {
+      final a = new js.Proxy.withArgList(js.context.Date, [1995,11,17,3,24,0]);
+      expect(a.getTime(), equals(819167040000));
+    });
+  });
+
+  test('js instanciation : new Object()', () {
     js.scoped(() {
       final a = new js.Proxy(js.context.Object);
       expect(a, isNotNull);
 
       a.attr = "value";
       expect(a.attr, equals("value"));
+    });
+  });
+
+  test(r'js instanciation : new RegExp("^\w+$")', () {
+    js.scoped(() {
+      final a = new js.Proxy(js.context.RegExp, r'^\w+$');
+      expect(a, isNotNull);
+      expect(a.test('true'), isTrue);
+      expect(a.test(' false'), isFalse);
     });
   });
 
