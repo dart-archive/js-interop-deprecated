a5a618351346b5024b6b6ce9203ca8f4fdbf0db2
Gubaer <karl.guggisberg@guggis.ch>
2012-12-09 13:26:16 +0100

    added 'functionProxy(f)'

--- test/browser_tests.dart
+++ test/browser_tests.dart
@@ -354,4 +354,23 @@ main() {
       expect(foo.getAge(), equals(10));
     });
   });
+  
+  
+  group("function proxy", () {
+    test("- function must not be null", () {
+      expect(() => js.functionProxy(null), throwsA(new isInstanceOf<ArgumentError>()));
+    });
+    
+    test("- function must not be a dart funtion", () {
+      dart_function(){};
+      expect(() => js.functionProxy(dart_function), throwsA(new isInstanceOf<ArgumentError>()));
+    });
+    
+    test('- can access a property of a function', () {
+      js.scoped(() {
+        final Bar = js.context.Bar;
+        expect(js.functionProxy(Bar).foo, "property_value");
+      });
+    });
+  });  
 }
