f11d20da5ce8f5aa92e986d42186c0b4742bed2e
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2013-08-17 19:14:20 +0200

    add dartdoc generation for js_wrapping

--- update-ghpages.dart
+++ update-ghpages.dart
@@ -7,7 +7,8 @@ import 'package:ghpages_generator/ghpages_generator.dart' as gh;
  */
 main() {
   new gh.Generator()
-  ..setDartDoc(['lib/js.dart'], excludedLibs: ['metadata'], outDir: 'docs')
+  ..setDartDoc(['lib/js.dart', 'lib/js_wrapping.dart'],
+      excludedLibs: ['metadata', 'meta'], outDir: 'docs')
   ..setExamples(true)
   ..templateDir = 'gh-pages-template'
   ..generate();
