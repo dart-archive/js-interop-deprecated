9e5dddc3828f6411a12e3ca2de1d190b2724ae5c
Justin Fagnani <justin@fagnani.com>
2014-08-13 13:51:28 -0700

    Initial check in of metadata classes, JsInterface, the library transformer, and proxy generator.
    Change-Id: Ie38c631319257a85648287b74859ae1ddfbe9899

---
+++ test/transformer/test_sources/test.dart
@@ -0,0 +1,93 @@
+library js.transformer_test;
+
+//import 'dart:js' as js;
+
+import 'package:js/js.dart';
+
+//part 'part.dart';
+
+@JsGlobal()
+abstract class Context extends JsInterface {
+
+  factory Context() {}
+
+  Context._create();
+
+  JsFoo foo; // read a typed JS object from JS
+
+  ExportMe exportMe; // write a exported Dart object to JS
+
+}
+
+@JsConstructor('JsThing')
+abstract class JsFoo extends JsInterface {
+
+  factory JsFoo(String name) {}
+
+  JsFoo._create();
+
+  String name;
+
+  int y() => 1;
+
+  JsBar bar;
+
+  JsBar getBar(JsBar b);
+
+}
+
+@JsConstructor('JsThing2')
+abstract class JsBar extends JsInterface {
+
+  int y;
+
+}
+
+@Export()
+class ExportMe {
+
+  static String staticField = 'a';
+  static bool staticMethod() => false;
+
+  String field;
+
+  int method() => 42;
+
+  bool get getter => true;
+
+  String _privateMethod() => "privateMethod";
+
+  String _privateField = "privateField";
+
+  void optionalArgs(a, [b, c]) {
+    print("a: $a");
+    print("b: $b");
+    print("c: $c");
+  }
+
+  void namedArgs(a, {b, c}) {
+    print("a: $a");
+    print("b: $b");
+    print("c: $c");
+  }
+
+}
+
+@NoExport()
+class DoNotExport {
+  String field;
+  bool get getter => false;
+}
+
+String topLevelField = "aardvark";
+
+@Export()
+DoNotExport getDoNotExport() => new DoNotExport();
+
+@NoExport()
+void main() {
+//  print('hello world');
+//  print("window: ${js.context['window']}");
+//  var o1 = new ExportMe()..field = 'created in Dart';
+//  js.context['o1'] = o1;
+}
